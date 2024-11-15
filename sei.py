1d0a7abfaad39d301f084e29dcb804746d91481d
wilkersilva101 <wilkerwanderley@hotmail.com>
2024-11-15 20:52:13 -0300

    Add files via upload

--- sei.py
+++ sei.py
@@ -1,42 +1,61 @@
-#automatizar (gestão de contratos)
-#1 acessar o sei de teste____________________________________________________________ok
-#2 identificar um processo___________________________________________________________ok
-#2.1 buscar (CONTRATO Nº 306/2023)___________________________________________________ok
-#2.2 deve ser espeficifcado um filtro para identificação do processoç_________________
-#3 criar ou identificar acompanhamento especial referente ao grupo de processos______
-#4 adicinar ao processo do item2 ao item 3
 from time import sleep
-
+from IPython.display import display
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Configurar o Pandas para exibir todas as colunas
+pd.set_option('display.max_columns', None)
 
 browser = webdriver.Chrome()
 # Acesso ao sistema SEI TJPI
 browser.get('https://www.tjpi.jus.br/pesquisas')
+
+# Esperar até que o campo de usuário esteja presente
+WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.NAME, "user[username]"))
+)
+
 elem = browser.find_element(By.NAME, "user[username]")
 elem.send_keys("wilker.silva")
 elem = browser.find_element(By.NAME, "user[password]")
 elem.send_keys("EAgames2019")
-# clicar no botão Acessa
+
+# Clicar no botão Acessa
 elem.send_keys(Keys.RETURN)
 
-# Clicar no link pesquisas/surveys
+# Esperar até que o link "147" esteja presente
+WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.LINK_TEXT, "147"))
+)
+
 elem = browser.find_element(By.LINK_TEXT, "147")
-
-# Clicar no botão Pesquisar Admin pegar o xpath
-#elem = browser.find_element(By.XPATH, "//a[@href='https://www.tjpi.jus.br/pesquisas/surveys']")
-
-
 elem.click()
 
+# Esperar até que o link "Admin" esteja presente
+WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.LINK_TEXT, "Admin"))
+)
 
+elem = browser.find_element(By.LINK_TEXT, "Admin")
+elem.click()
 
+# Esperar até que o link "Responder novamente" esteja presente
+WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.LINK_TEXT, "Responder novamente"))
+)
 
+elem = browser.find_element(By.LINK_TEXT, "Responder novamente")
+elem.click()
 
+# Importar a lista de servidores
+servidores = pd.read_csv("importar.csv", sep=",")
+
+# Montar o dataframe
+df = pd.DataFrame(servidores)
+display(df)
 
 sleep(50)
-
-
-#//*[@id="participant_unity"]
